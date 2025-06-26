d54cff4ca939b5b99be2506e98eba231adb608a1
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-06-26 13:48:24 +0100

    fix: changed app name

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -35,7 +35,7 @@ export function Navbar() {
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-8">
             <Link href="/" className="text-xl font-bold text-gray-900">
-              BlogApp
+              Urban Vibes
             </Link>
             <div className="hidden md:flex space-x-4">
               <Link href="/">
